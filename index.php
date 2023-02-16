b9789a74be2b02f11cca40f8b04364664608c509
shahnewazsakil <shahnewaz587@gamil.com>
2023-02-16 19:12:34 +0600

    added

--- index.php
+++ index.php
@@ -1,20 +1,72 @@
 <?php
 
-function checkNumber($n){
-    if($n % 2 == 0){
-        return "Even Number";
-    }else{
-        return "Odd Number";
-    }
-}
+// use serialize, unserialize,  json_encode , json_decode function print the results
+$emp = [
+    'john' => [
+        'age' => 20,
+        'salary' => 10000,
+        'address' => 'Dhaka',
+        'blod_group' => 'o+'
+    ],
+    'harry' => [
+        'age' => 30,
+        'salary' => 15000,
+        'address' => 'London',
+        'blod_group' => 'B-'
+    ],
+    'mark' => [
+        'age' => 25,
+        'salary' => 14000,
+        'address' => 'America',
+        'blod_group' => 'o-'
+    ],
+    'chirs' => [
+        'age' => 23,
+        'salary' => 19000,
+        'address' => 'India',
+        'blod_group' => 'A+'
+    ],
+];
 
-echo checkNumber(5);
+// $new = serialize($emp);
 
-echo "<br>";
+// print_r($new);
+// // unserialize($new);
 
-$sum = 0;
-for($i=0; $i < 101; $i++){
-    $sum += $i;
-}
+// $json = json_encode($emp);
 
-echo $sum;
+// $jsd = json_decode($json);
+
+// remove address key and value from the array
+$emp = [
+    'john' => [
+        'age' => 20,
+        'salary' => 10000,
+        'address' => 'Dhaka',
+        'blod_group' => 'o+'
+    ]
+];
+
+// extract green blue white value from the array.
+$arr = ["red", "green", "blue", "white", "violet"];
+
+
+// extract green blue white value from the array and insert $newValue array 
+$newValue = ["Pink", "Orange", "Yellow"];
+$arr = ["red", "green", "blue", "white", "violet"];
+
+
+
+
+// insert $newArr array values to existing array
+$newArr = ["Pink", "Orange", "Yellow"];
+$oldArr = ["red", "green", "blue", "white", "violet"];
+
+
+
+// find the extension of a photo.
+// output png
+$str = "myphoto.old.new.png";
+
+// find the position of PHP. Case-sensitive
+$str = "Hi, My name is Rahim. I love PHP. I love php also";
