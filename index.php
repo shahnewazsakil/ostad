619f6e1a4c99677cd184527b8af237371bee5427
shahnewazsakil <shahnewaz587@gamil.com>
2023-02-21 19:44:41 +0600

    added

--- index.php
+++ index.php
@@ -85,3 +85,57 @@
 // }
 
 // echo vowel("Hi, i am a developer");
+
+
+if(isset($_FILES['photo']) && !empty($_FILES['photo']['name'])){
+    
+    $photo_name = $_FILES['photo']['name'];
+    $photo_tmp_name = $_FILES['photo']['tmp_name'];
+    $photo_ext = $_FILES['photo']['type'];
+    $photo_size = $_FILES['photo']['size'];
+
+    $allow_ext = array('png', 'jpg');
+
+    $photo_ext_name = explode('.' , $photo_name);
+
+    $photo_ext_final = end($photo_ext_name);
+
+    if(in_array($photo_ext_final, $allow_ext) == false){
+        echo "File extension is not allowed";
+        return false;
+    }
+
+    if($photo_size > 2097152){
+        echo "File size must be lower than 2mb";
+        return false;
+    }
+
+    $date = date('Y-m-d_H-i-s');
+
+    $photo_final_name = $date . "-" . basename($photo_name);
+
+    if(move_uploaded_file($photo_tmp_name, "{$photo_final_name}")){
+        echo "file uploaded";
+    }else{
+        echo "file upload failed";
+    }
+    
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="<?php $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
+        <input type="file" name="photo">
+        <button type="submit">upload file</button>
+    </form>
+</body>
+</html>
